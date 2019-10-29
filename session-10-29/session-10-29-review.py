38230ae1e33b7bcc1ef11810f67de21b742c10c4
Lynette Jeem <lynettemjeem@gmail.com>
2019-10-29 17:57:31 -0400

    Practice with Arrays

--- session-10-29/session-10-29-review.py
+++ session-10-29/session-10-29-review.py
@@ -2,23 +2,30 @@
 
 # Reviewing While loops:
 # Write code to print multiples of 5 from 45 to 90
+i = 40
+while i <= 85:
+	i+= 5
+	print(i)
+
 
 
 # With your partner: discuss: What is a set? What is it good for?
 # Write your response as a comment here:
-#
+#a set is similar to a list but instead the data isn't in order and rather dumped data 
 
 # Create a variable called "myset" and put whatever you want in it:
 
+myset = {11,2003,"november","Lynette","Jeem",8}
 
+# Create a 2D numpy array of size 3x5 with numbers 1-15:
 
-# Create a 2D numpy array of size 3x5 with numbers 1-5:
-
-
+import numpy as np 
+a = np.arange(1,16,1).reshape(3,5)
+print(a)
 
 # From your array, print the number 7:
 
-
+print(a[1,1])
 
 # Challenge: use while loop to print the following pattern:
 # ****
