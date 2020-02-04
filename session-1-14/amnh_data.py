0259be5a5e6b8b54aa1aa1a7b2458fbd2c7aa8dc
Lynette Jeem <lynettemjeem@gmail.com>
2020-02-04 17:57:13 -0500

    i'm upfating

--- session-1-14/amnh_data.py
+++ session-1-14/amnh_data.py
@@ -13,6 +13,7 @@ with warnings.catch_warnings():
 	import glob
 	import pathlib
 
+#importing libraries needed 
 
 print('ready') 
 
@@ -26,27 +27,33 @@ def show_batch(image_batch, label_batch):
       plt.axis('off')
   plt.show()
 
+  #shows the images 
+
 AUTOTUNE = tf.data.experimental.AUTOTUNE
 
-data_dir = '/Users/helenfellow/Documents/Education /cnn_data'
+data_dir = '/Users/brownscholar/Desktop/co2_data'
 data_dir = pathlib.Path(data_dir)
+#directs the path to my data 
 
 image_count = len(list(data_dir.glob('*/*.jpg')))
 print(image_count)
 
+#counts the number of images 
 
 CLASS_NAMES = np.array([item.name for item in data_dir.glob('*') if item.name != ".DS_Store"])
 print(CLASS_NAMES)
+
 
 seal_1 = list(data_dir.glob('seal_1/*'))
 
 
 image_generator = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
-BATCH_SIZE = 622
-IMG_HEIGHT = 1024
-IMG_WIDTH = 768
+BATCH_SIZE = 310
+IMG_HEIGHT = 3024
+IMG_WIDTH = 3024
 STEPS_PER_EPOCH = np.ceil(image_count/BATCH_SIZE)
 
+#generates image
 
 train_data_gen = image_generator.flow_from_directory(directory=str(data_dir),
                                                      batch_size=BATCH_SIZE,
@@ -59,7 +66,7 @@ image_batch, label_batch = next(train_data_gen)
 show_batch(image_batch, label_batch)
 
 model = models.Sequential()
-model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(1024, 768, 3)))
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(IMG_HEIGHT,IMG_WIDTH, 3)))
 model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Conv2D(64, (3, 3), activation='relu'))
 model.add(layers.MaxPooling2D((2, 2)))
@@ -67,6 +74,7 @@ model.add(layers.Conv2D(64, (3, 3), activation='relu'))
 model.add(layers.Flatten())
 model.add(layers.Dense(64, activation='relu'))
 model.add(layers.Dense(10, activation='softmax'))
+#maxpooling the layers 
 
 print(model.summary())
 
--- session-12-10/tensorflow_tutorial.py
+++ session-12-10/tensorflow_tutorial.py
@@ -45,3 +45,32 @@ model.add(layers.Conv2D(64, (3, 3), activation='relu'))
 model.summary()
 
 #displays the model of the dataset 
+model.add(layers.Flatten())
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(10, activation='softmax'))
+
+#makes an activation by unrolling the 3D set data to 1D and adds more layers on top
+
+model.summary()
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+history = model.fit(train_images, train_labels, epochs=10, 
+                    validation_data=(test_images, test_labels))
+
+# #comment here about this code: Translate the loss of the data set after it is being runned
+
+# plt.plot(history.history['accuracy'], label='accuracy')
+# plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+# plt.xlabel('Epoch')
+# plt.ylabel('Accuracy')
+# plt.ylim([0.5, 1])
+# plt.legend(loc='lower right')
+
+# test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+
+# #plots the data set after findin out its loss and updating its accuracy
+
+# print(test_acc)
