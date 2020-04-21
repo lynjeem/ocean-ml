ea60b540f397d4d0c1aaea161fe1c3e3817d592b
Meera Desai <meera.ann.desai@gmail.com>
2020-04-21 12:57:52 -0700

    added changes

---
+++ session-4-21/hovmoller-plot-start.py
@@ -0,0 +1,7 @@
+from netCDF4 import Dataset
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import cm
+from matplotlib.colors import ListedColormap, LinearSegmentedColormap
+
+
